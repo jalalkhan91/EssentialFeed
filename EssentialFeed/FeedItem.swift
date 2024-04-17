5ba23c14bedff88f5061b35221acb1bce0df0a34
JalalKhan <jalal.khan.91@hotmail.com>
2024-04-17 21:21:10 +0200

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jalal Khan on 17.04.24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
